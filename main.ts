8b68dbfe26dc930d0b169211e857598b1b5df274
ChanisaPp <135142930+ChanisaPp@users.noreply.github.com>
2023-05-31 21:49:13 +0700

    Initial files for MakeCode project

---
+++ main.ts
@@ -0,0 +1,56 @@
+let option = 0
+for (let index = 0; index < 5; index++) {
+    option = randint(0, 6)
+    if (option == 1) {
+        basic.showLeds(`
+            . . . . .
+            . . . . .
+            . . # . .
+            . . . . .
+            . . . . .
+            `)
+    } else if (option == 2) {
+        basic.showLeds(`
+            . . . . .
+            . . . # .
+            . . . . .
+            . # . . .
+            . . . . .
+            `)
+    } else if (option == 3) {
+        basic.showLeds(`
+            . . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . .
+            `)
+    } else if (option == 4) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . . . .
+            . . . . .
+            # . . . #
+            `)
+    } else if (option == 5) {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            . . # . .
+            . . . . .
+            # . . . #
+            `)
+    } else {
+        basic.showLeds(`
+            # . . . #
+            . . . . .
+            # . . . #
+            . . . . .
+            # . . . #
+            `)
+    }
+}
+basic.forever(function () {
+	
+})
